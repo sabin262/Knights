5e27399a540e9324d80c1c169a04be4c4ddbd340
sabin262 <sabin262@gmail.com>
2017-02-04 08:35:46 +0545

    Card View implementation

---
+++ app/src/main/java/com/venomxc45/androidtest/MainPageFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.venomxc45.androidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPageFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page_fragment);
+    }
+}
